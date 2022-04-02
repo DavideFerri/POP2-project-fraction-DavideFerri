4a3d43ffc59be48ff1f2e859f200c83a8a2b87a2
DAVIDE <davide.ferri.94@gmail.com>
2022-04-02 18:09:41 +0200

    Further clean the API

--- src/fraction/FractionImpl.java
+++ src/fraction/FractionImpl.java
@@ -6,6 +6,10 @@ package fraction;
 import static java.lang.Math.max;
 
 public class FractionImpl implements Fraction {
+
+    private final int numerator;
+    private final int denominator;
+
     /**
      * Parameters are the <em>numerator</em> and the <em>denominator</em>.
      * Normalize the fraction as you create it.
@@ -14,12 +18,9 @@ public class FractionImpl implements Fraction {
      *
      * The constructor should throw an <pre>ArithmeticException</pre> if the denominator is zero.
      *
-     * @param numerator
-     * @param denominator
+     * @param numerator numerator of fraction
+     * @param denominator denominator of fraction
      */
-    private final int numerator;
-    private final int denominator;
-
     public FractionImpl(int numerator, int denominator) throws ArithmeticException
     {
         this(numerator+"/"+denominator);
@@ -30,7 +31,8 @@ public class FractionImpl implements Fraction {
      *
      * @param wholeNumber representing the numerator
      */
-    public FractionImpl(int wholeNumber) {
+    public FractionImpl(int wholeNumber)
+    {
         this(Integer.toString(wholeNumber));
     }
 
@@ -38,6 +40,8 @@ public class FractionImpl implements Fraction {
      * The parameter is a <pre>String</pre> containing either a whole number, such as `5` or `-3`, or a fraction,
      * such as "8/-12".
      * Allow blanks around (but not within) integers.
+     * The constructor should throw a <pre>NumberFormatException</pre> if there are blanks within integers or
+     * if the string does not represent a valid fraction.
      * The constructor should throw an <pre>ArithmeticException</pre>
      * if given a string representing a fraction whose denominator is zero.
      * <p>
@@ -49,27 +53,24 @@ public class FractionImpl implements Fraction {
     {
         int num;
         int den;
-        try
+        if (fraction.contains("/"))
         {
-            if (fraction.contains("/"))
-            {
-                String[] arr = fraction.split("/");
-                if (arr.length != 2) throw new NumberFormatException("Incorrect string format");
-                num = Integer.parseInt(arr[0].trim());
-                den = Integer.parseInt(arr[1].trim());
-            }
-            else
-            {
-                num = Integer.parseInt(fraction.trim());
-                den = 1;
-            }
+            // split by / and parse integers
+            String[] arr = fraction.split("/");
+            if (arr.length != 2) throw new NumberFormatException("Incorrect string format");
+            num = Integer.parseInt(arr[0].trim());
+            den = Integer.parseInt(arr[1].trim());
         }
-        catch(NumberFormatException e)
+        else
         {
-            throw e;
+            // parse numerator and set default for denominator
+            num = Integer.parseInt(fraction.trim());
+            den = 1;
         }
         if (den==0) throw new ArithmeticException("Divide by zero");
+        // get greatest commond divisor
         int gcd = FractionImpl.gcd(Math.abs(num),Math.abs(den));
+        // check if positive fraction and set members accordingly
         boolean is_positive = FractionImpl.isPositive(num,den);
         this.numerator = (is_positive) ? (Math.abs(num)/gcd):(-Math.abs(num)/gcd);
         this.denominator = Math.abs(den)/gcd;
@@ -143,10 +144,10 @@ public class FractionImpl implements Fraction {
      */
     @Override
     public boolean equals(Object obj) {
-        if (!(obj instanceof Fraction)) return false;
+        if (!(obj instanceof Fraction f)) return false;
         else
         {
-            Fraction f = (Fraction) obj;
+            // equals if subtraction returns 0
             return this.subtract(f).toString().equals("0");
         }
     }
@@ -172,6 +173,7 @@ public class FractionImpl implements Fraction {
      */
     @Override
     public int compareTo(Fraction o) {
+        // use subtraction to compare
         FractionImpl sub = new FractionImpl(this.subtract(o).toString());
         return sub.numerator;
     }
@@ -183,23 +185,41 @@ public class FractionImpl implements Fraction {
     public String toString()
     {
         if (this.denominator==1) return Integer.toString(this.numerator);
-        else return Integer.toString(this.numerator)+"/"+Integer.toString(this.denominator);
+        else return this.numerator +"/"+ this.denominator;
     }
 
-    private static int gcd(int numerator, int denominator)
+    /**
+     * Find the greatest common divisor of two positive numbers.
+     * For instance, if a is <pre>6</pre> and b is
+     * <pre>9</pre> the method should return
+     * <pre>3</pre>
+     *
+     * @param a first int
+     * @param b second int
+     * @return the greatest common divisor
+     */
+    private static int gcd(int a, int b)
     {
-        while (numerator > 0 && denominator > 0)
+        while (a > 0 && b > 0)
         {
-            if (numerator >= denominator)
-                numerator = numerator % denominator;
-            else denominator = denominator % numerator;
+            // replace larger number with the remainder of dividing the larger by the smaller
+            if (a >= b)
+                a = a % b;
+            else b = b % a;
         }
-        return max(numerator,denominator);
+        return max(a,b);
     }
 
+    /**
+     * Parameters are the <em>numerator</em> and the <em>denominator</em>.
+     * Find whether a fraction is positive or not.
+     *
+     * @param numerator numerator of fraction
+     * @param denominator denominator of fraction
+     * @return true if greater or equal than 0
+     */
     private static boolean isPositive(int numerator, int denominator)
     {
-        if (numerator*denominator>=0) return true;
-        else return false;
+        return numerator * denominator >= 0;
     }
 }
